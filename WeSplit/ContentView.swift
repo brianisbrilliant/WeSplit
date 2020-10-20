6132da71b29a315af8dd1bf0acbbb6bbcb648bde
Brian Foster <brianisbrilliant@gmail.com>
2020-10-20 13:39:33 -0500

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Brian Foster on 10/20/20.
+//  Copyright © 2020 Brian Foster. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
